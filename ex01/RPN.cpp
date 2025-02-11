ac25864ee11290d4a6438274bb42b74010724d6b
Manutea Birou <mbirou@42angouleme.fr>
2025-02-11 09:31:14 +0100

    need to fix 06

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: mbirou <mbirou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 15:04:20 by mbirou            #+#    #+#             */
-/*   Updated: 2025/02/10 11:18:16 by mbirou           ###   ########.fr       */
+/*   Updated: 2025/02/10 17:30:09 by mbirou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ std::stack<std::string>	split(const std::string &expression)
 	for (std::size_t pos = 0; pos < expression.length(); pos = expression.find_first_not_of("0123456789./*-+", pos))
 	{
 		PRINT pos AND ", " AND expression.substr(pos, expression.find_first_not_of("0123456789./*-+", pos + 1) + pos) ENDL;
-		split.push(expression.substr(pos + (pos > 0), expression.find_first_not_of("0123456789./*-+", pos + 1) + pos));
+		rsplit.push(expression.substr(pos + (pos > 0), expression.find_first_not_of("0123456789./*-+", pos + 1) + pos));
 		pos += 1;
 	}
-	split.push("");
+	rsplit.push("");
+	for (std::string exp = rsplit.top(); rsplit.size() > 0; exp = rsplit.top(), rsplit.pop())
+	{
+		split.push(exp);
+	}
 	return (split);
 }
 
